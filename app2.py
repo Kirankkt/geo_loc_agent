839535194c628e3d799c4cd383c6ea34dc488147
Kiran <128660185+Kirankkt@users.noreply.github.com>
2024-12-17 16:43:42 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -40,7 +40,8 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Display uploaded image
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+
 
     # Convert image to bytes
     image_bytes = uploaded_file.getvalue()
